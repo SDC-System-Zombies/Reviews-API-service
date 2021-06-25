7bdc570388971407a07601327b48ce7209172e25
Yichun Jin <jinyichun17@gmail.com>
2021-06-24 21:19:55 -0700

    Complete Define your database queries

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -66,6 +66,34 @@ module.exports = {
         callback(null, result);
       })
       .catch((err) => {console.log('Query Meta Error: ' + err); callback(404)});
+  },
+
+  post: (body, callback) => {
+    const queryStr = `INSERT INTO reviews (id, product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email) VALUES
+    (DEFAULT, ${body.product_id}, ${body.rating}, CURRENT_DATE, ${body.summary}, ${body.body}, ${body.recommend}, ${body.name}, ${body.reviewer_email});`;
+    pool.query(queryStr)
+      .then((data) => {
+        callback(null, data);
+      })
+      .catch((err) => {console.log('insert table error:' + err); callback(err);});
+  },
+
+  put: (endpoint, review_id, callback) => {
+    let col;
+    let queryStr;
+    if (endpoint === 'helpful') {
+      queryStr = `UPDATE reviews SET helpfulness = helpfulness + 1 WHERE id = ${review_id}`;
+    }
+    if (endpoint === 'report') {
+      queryStr = `UPDATE reviews SET reported = true WHERE id = ${review_id}`;
+    }
+
+    pool.query(queryStr)
+    .then((data) => {
+      callback(null, data);
+    })
+    .catch((err) => {console.log('insert table error:' + err); callback(err);});
+
   }
 
 }
