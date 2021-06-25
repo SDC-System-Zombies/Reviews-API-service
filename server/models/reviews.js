18d1ca89fb5aaee5d6f8510757127d5150f3244e
Yichun Jin <jinyichun17@gmail.com>
2021-06-24 20:12:16 -0700

    Added getAll and getMeta endpoint and query func

---
+++ server/models/reviews.js
@@ -0,0 +1,73 @@
+const pool = require('../databases/postgres');
+// console.log(pool);
+
+module.exports = {
+
+  getAll: (productId, sort, callback) => {
+
+    const queryStr = `SELECT photos.review_id, reviews.rating, reviews.summary, reviews.recommend, reviews.response, reviews.body, reviews.body, reviews.date, reviews.reviewer_name, reviews.helpfulness, photos.id, photos.url FROM reviews JOIN photos ON reviews.id = photos.review_id
+    WHERE reviews.product_id = ${productId} ORDER BY ${sort} DESC;`;
+
+    pool.query(queryStr)
+      .then((data) => {
+        let detail={};
+        detail.product = productId;
+        detail.results = [];
+        let results = [];
+        let photos = [];
+        let reviewIds = [];
+        for (let i of data.rows) {
+          photos.push({id: i.id, url: i.url});
+          if (reviewIds.indexOf(i.review_id) < 0) {
+            reviewIds.push(i.review_id);
+            delete i.id;
+            delete i.url;
+            i.photos = photos;
+            results.push(i);
+          }
+        }
+        detail.results = results;
+
+        callback(null, detail);
+      })
+      .catch((err) => {console.log('Query All Error: ' + err); callback(404)});
+  },
+
+  getMeta: (productId, callback) => {
+
+    const queryStr = `SELECT reviews.rating, reviews.recommend, chars.name, charReviews.characteristic_id, charReviews.value FROM chars
+      JOIN charReviews ON chars.id = charReviews.characteristic_id
+      JOIN reviews ON reviews.id = charReviews.review_id
+      WHERE chars.product_id = ${productId};`;
+
+    pool.query(queryStr)
+      .then((data) => {
+        let result = {product_id: productId, rating: {}, recommended: {}};
+        let characteristics = {};
+        for (let i of data.rows) {
+          if (result.rating[i.rating]) {
+            result.rating[i.rating]++;
+          } else {
+            result.rating[i.rating] = 1;
+          }
+          if (result.recommended[i.recommend]) {
+            result.recommended[i.recommend]++;
+          } else {
+            result.recommended[i.recommend] = 1;
+          }
+          if (characteristics[i.name]) {
+            let average = (characteristics[i.name].value + i.value) / 2;
+            characteristics[i.name].value = average;
+          } else {
+            characteristics[i.name] = {id: i.characteristic_id, value: i.value}
+          }
+        }
+        result.characteristics = characteristics;
+        callback(null, result);
+      })
+      .catch((err) => {console.log('Query Meta Error: ' + err); callback(404)});
+  }
+
+}
+
+// module.exports.getAll(13023, (data) => console.log(data.rows));
