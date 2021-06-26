904af8ff2d13d20bfd31ff5480a94570decec5eb
Yichun Jin <jinyichun17@gmail.com>
2021-06-26 08:56:36 -0700

    Update reviews collection

--- server/databases/mongodb/index.js
+++ server/databases/mongodb/index.js
@@ -1,14 +1,58 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/sdc', {useNewUrlParser: true, useUnifiedTopology: true});
 
-let reviewsSchema = new mongoose.Schema({
-  id: Number
+const reviewsSchema = new mongoose.Schema({
+  id: Number,
+  product_id: Number,
+  rating: Number,
+  date: Date,
+  summary: String,
+  body: String,
+  recommend: Boolean,
+  reported: Boolean,
+  reviewer_name: String,
+  reviewer_email: String,
+  response: String,
+  helpfulness: Number,
+  photos: [{
+    id: Number,
+    url: String
+  }]
 });
 
-let Reviews = mongoose.model('reviews', reviewsSchema);
+const photosSchema = new mongoose.Schema({
+  id: Number,
+  review_id: Number,
+  url: String
+});
 
-let find = (callback) => {
-  return Reviews.find({product_id:13023}).sort({stars:-1}).limit(25).exec(callback);
+const Reviews = mongoose.model('reviews', reviewsSchema);
+
+const Photos = mongoose.model('photos', photosSchema);
+
+
+const updateReviews = async (productId, callback) => {
+  for (let i = 1000000; i < 6144923; i++) {
+    let photos = [];
+    await Photos.find({review_id: i})
+      .then((data) => {
+        for (let i of data) {
+          photos.push({id: i.id, url: i.url});
+        }
+      })
+      .then((data) => {
+        Reviews.updateOne({id: i}, {photos: photos}, {upsert: false}, (err, data) => {
+          if (err) {console.log(err)}
+          console.log('update 1 record:' + i);
+        })
+      })
+      .catch((err) => console.log(err));
+  }
+
+}
+
+const find = () => {
+  console.log()
 }
 
 module.exports.find = find;
