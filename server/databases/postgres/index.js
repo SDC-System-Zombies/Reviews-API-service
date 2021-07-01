dcb573464bd52e8076583240aa9831614dcf2ac6
Yichun Jin <jinyichun17@gmail.com>
2021-07-01 14:54:57 -0700

    change psql host to loaclhost

--- server/databases/postgres/index.js
+++ server/databases/postgres/index.js
@@ -1,7 +1,7 @@
 const { Pool, Client } = require('pg');
 
 const pool = new Pool({
-  host: 'ec2-3-142-46-88.us-east-2.compute.amazonaws.com',
+  host: 'localhost',
   hostname: 'ubuntu',
   user: process.env.DB_USER || 'postgres',
   password: 'woshiyuzhoumeishaonv!',
